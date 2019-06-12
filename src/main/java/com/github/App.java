6316fad11bdb408281f36a59266cc8527a9d0cf8
lian-heng <it.lh@outlook.com>
2019-06-12 11:08:23 +0800

    HS-257

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan(basePackages="com.github.mapper")
 public class App {
-	
+	/**
+	 * 演示git仓库集成
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
